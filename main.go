f75a21375ce92037f34e1c55780234609ad7a5da
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-05-09 16:41:07 -0700

    Adding all other modified files...

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-	result := make([]string, 0)
-	splitSlice := strings.Fields(text)
-	for _, word := range splitSlice {
-		result = append(result, strings.ToLower(word))
-	}
-	//result = append(result, strings.Split(text, " "))
-
-	return result
-}
-
 func main() {
-	fmt.Print("Hello, World! \n")
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	userInput := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		userInput.Scan()
+
+		userText := cleanInput(userInput.Text())
+		if len(userText) == 0 {
+			continue
+		}
+
+		commandName := userText[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
