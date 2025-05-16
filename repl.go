3a13967cb00c2b4b99bd127b263e99eded7d8c6f
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-05-16 15:30:19 -0700

    Started implementing commands.

--- repl.go
+++ repl.go
@@ -7,6 +7,20 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandList = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
 func startRepl() {
 	userInput := bufio.NewScanner(os.Stdin)
 	for {
@@ -18,9 +32,16 @@ func startRepl() {
 			continue
 		}
 
-		commandName := userText[0]
-		fmt.Printf("Your command was: %s\n", commandName)
-
+		command := userText[0]
+		functionName, ok := commandList[command]
+		if !ok {
+			fmt.Printf("Unknown command: %s\n", command)
+		} else {
+			err := functionName.callback()
+			if err != nil {
+				return
+			}
+		}
 	}
 }
 
@@ -28,4 +49,10 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
