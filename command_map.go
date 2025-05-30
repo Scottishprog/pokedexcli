87105abcdc50769ca819bb8f5951baccc39fbf40
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-05-30 15:06:20 -0700

    Got the basic API functionality working, can pull and print a list of 20 locations.

--- command_map.go
+++ command_map.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,13 @@ import (
 
 func commandMap() error {
 	// get map information from:
+
+	// get this code working - produces a named struct that can be stepped through to print out all the map
+	// locations.
+	// once this is done, pull all API related code to an internal package, this func will
+	// only deal with requesting the struct, and display.
+	// An additional headache is to to get the Next/Previous struct parameters working with the
+	// function callback - AFTER getting the GET code/formatting working!
 
 	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
 	if err != nil {
@@ -22,8 +30,28 @@ func commandMap() error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", body)
 
+	type mapData struct {
+		Count    int    `json:"count"`
+		Next     string `json:"next"`
+		Previous string `json:"previous"`
+		Results  []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"results"`
+	}
+
+	mapdata := mapData{}
+	err = json.Unmarshal(body, &mapdata)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Next: ", mapdata.Next, "\nPrevious: ", mapdata.Previous)
+	for _, result := range mapdata.Results {
+		fmt.Println(result.Name)
+	}
+
+	//fmt.Println(mapdata)
 	return nil
 
 }
