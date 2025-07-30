a1aee90d47f19d01f2daed07171be0db9e0b5e0d
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-07-29 21:35:03 -0700

    Finally got the base map functionality working - both forwards and backwards. Next is cacheing of results.

--- command_map.go
+++ command_map.go
@@ -12,8 +12,29 @@ func commandMap(cfg *config) error {
 
 	cfg.next = shallowmapdata.Next
 	cfg.previous = shallowmapdata.Previous
+	
+	for _, result := range shallowmapdata.Results {
+		fmt.Println(result.Name)
+	}
 
-	fmt.Println("Next: ", &cfg.next, "\nPrevious: ", shallowmapdata.Previous)
+	return nil
+
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.previous == nil {
+		fmt.Println("On the first page")
+		return nil
+	}
+
+	shallowmapdata, err := cfg.pokeapiClient.LocationList(cfg.previous)
+	if err != nil {
+		return err
+	}
+
+	cfg.next = shallowmapdata.Next
+	cfg.previous = shallowmapdata.Previous
+
 	for _, result := range shallowmapdata.Results {
 		fmt.Println(result.Name)
 	}
--- command_mapb.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func commandMapb(Config *config) error {
-
-	return nil
-}
